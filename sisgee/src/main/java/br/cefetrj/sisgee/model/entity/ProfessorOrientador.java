e58283394a664dc4980a439ecafcf50cb7b8dc69
RicardoBucard <ricardobucard@gmail.com>
2019-06-14 13:39:21 -0300

    Adicionada edição de data início de termo aditivo

--- sisgee/src/main/java/br/cefetrj/sisgee/model/entity/ProfessorOrientador.java
+++ sisgee/src/main/java/br/cefetrj/sisgee/model/entity/ProfessorOrientador.java
@@ -2,10 +2,8 @@ package br.cefetrj.sisgee.model.entity;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -20,7 +18,7 @@ public class ProfessorOrientador {
 	@Column(length = 80, nullable = false)
 	private String nomeProfessorOrientador;
 
-	@OneToMany(mappedBy = "professorOrientador", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "professorOrientador")
 	private List<TermoEstagio> termoEstagios;
 
 	public ProfessorOrientador() {}
