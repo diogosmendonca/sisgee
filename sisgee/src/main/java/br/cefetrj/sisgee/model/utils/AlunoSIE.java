514a77b134c2e352fa509254493afff1f4d2b539
padu <padu@padu-desktop>
2019-09-16 22:29:13 -0300

    Inclusão das dependências no POM para o uso do JSON, Criação da Classe de apoio para o parse de aluno proveniente do SIE e inclusão da classe de constantes referenciada no commit anterior

---
+++ sisgee/src/main/java/br/cefetrj/sisgee/model/utils/AlunoSIE.java
@@ -0,0 +1,55 @@
+package br.cefetrj.sisgee.model.utils;
+
+public class AlunoSIE {	
+	
+	private String matricula;
+	private String nome;
+	private String cpf;
+	private String curso;
+	private String siglaCurso;
+	private String tipo;
+	private String campus;
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getSiglaCurso() {
+		return siglaCurso;
+	}
+	public void setSiglaCurso(String siglaCurso) {
+		this.siglaCurso = siglaCurso;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCampus() {
+		return campus;
+	}
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}	
+}
---
+++ sisgee/src/main/java/br/cefetrj/sisgee/model/utils/Constantes.java
@@ -0,0 +1,6 @@
+package br.cefetrj.sisgee.model.utils;
+
+public class Constantes {
+	public static final String ATIVO = "Ativo";
+	public static final String ENCERRADO = "Encerrado";
+}
