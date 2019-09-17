65ec42d52b25340a44a6d067bdd6bf5489022f2f
padu <padu@padu-desktop>
2019-09-16 22:23:56 -0300

    Correção do bug reportado na issue #19: Estado do Termo Estágio, tornando ele calculado de acordo com os campos data de fim ou rescisão; Criação de uma classe de constantes para armazenar o valor da String do estado de Termo Estágio; Alteração do comportamento da busca de termo aditivo, para ocultar o botão de encerrar termo caso não haja termo ativo e alteração na busca de aluno para adequar a essa lógica

--- sisgee/src/main/java/br/cefetrj/sisgee/view/termoaditivo/BuscaTermoAditivoServlet.java
+++ sisgee/src/main/java/br/cefetrj/sisgee/view/termoaditivo/BuscaTermoAditivoServlet.java
@@ -13,14 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.cefetrj.sisgee.control.AlunoServices;
-import br.cefetrj.sisgee.control.TermoAditivoServices;
 import br.cefetrj.sisgee.model.entity.Aluno;
 import br.cefetrj.sisgee.model.entity.TermoAditivo;
 import br.cefetrj.sisgee.model.entity.TermoEstagio;
+import br.cefetrj.sisgee.model.utils.Constantes;
 import br.cefetrj.sisgee.view.utils.ServletUtils;
 import br.cefetrj.sisgee.view.utils.ValidaUtils;
+
 /**
  * Servlet responsável pela busca de informações de cada termo aditivo
+ * 
  * @author Vinicius Paradellas
  * @since 1.1
  *
@@ -30,56 +32,69 @@ import br.cefetrj.sisgee.view.utils.ValidaUtils;
 public class BuscaTermoAditivoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-        /**
-         * 
-        * @param request um objeto HttpServletRequest que contém a solicitação feita pelo cliente do servlet.
-        * @param response um objeto HttpServletResponse que contém a resposta que o servlet envia para o cliente
-        * @throws ServletException se o pedido do service não puder ser tratado
-        * @throws IOException se um erro de entrada ou saída for detectado quando o servlet manipula o pedido 
-         */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * 
+	 * @param request  um objeto HttpServletRequest que contém a solicitação feita
+	 *                 pelo cliente do servlet.
+	 * @param response um objeto HttpServletResponse que contém a resposta que o
+	 *                 servlet envia para o cliente
+	 * @throws ServletException se o pedido do service não puder ser tratado
+	 * @throws IOException      se um erro de entrada ou saída for detectado quando
+	 *                          o servlet manipula o pedido
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Locale locale = ServletUtils.getLocale(request);
 		ResourceBundle messages = ResourceBundle.getBundle("Messages", locale);
-		
+
 		String msg = null;
 		String idAluno = request.getParameter("idAluno");
-                
+
 		Integer id = null;
-                System.out.println("Aqui >>> "+idAluno);
-		if(Integer.parseInt(idAluno)!=-1){
-		msg = ValidaUtils.validaObrigatorio("Aluno", idAluno);		
-		if(msg.trim().isEmpty()) {
-			msg = ValidaUtils.validaInteger("Aluno", idAluno);
-			if(msg.trim().isEmpty()) {
-				id = Integer.parseInt(idAluno);					
-			}else {
+		boolean existeTermoAtivo = false;
+
+		if (Integer.parseInt(idAluno) != -1) {
+			msg = ValidaUtils.validaObrigatorio("Aluno", idAluno);
+			if (msg.trim().isEmpty()) {
+				msg = ValidaUtils.validaInteger("Aluno", idAluno);
+				if (msg.trim().isEmpty()) {
+					id = Integer.parseInt(idAluno);
+				} else {
+					msg = messages.getString(msg);
+				}
+			} else {
 				msg = messages.getString(msg);
 			}
-		} else {
-                    msg = messages.getString(msg);
+
+			Aluno aluno = AlunoServices.buscarAluno(new Aluno(id));
+			List<TermoEstagio> termoEstagios = aluno.getTermoEstagios();
+
+			// TODO consertar a lógica de mensagem vazia
+			if (msg != "") {
+				aluno = AlunoServices.buscarAluno(new Aluno(id));
+				termoEstagios = aluno.getTermoEstagios();
+			}
+
+			if (termoEstagios != null) {				
+				if (!aluno.getTermoEstagios().isEmpty()) {
+					List<TermoAditivo> termoAditivos = aluno.getTermoEstagios().get(aluno.getTermoEstagios().size() - 1)
+							.getTermosAditivos();
+					Collections.sort(termoAditivos);
+					request.setAttribute("termosAditivos", termoAditivos);
+				}
+
+				// Testando Se existe um termo de estágio ativo, caso haja,
+				// seta a variável para mostrar o botão de encerramento de termo.
+				for (TermoEstagio termoEstagio : termoEstagios) {
+					if (Constantes.ATIVO.equalsIgnoreCase(termoEstagio.getEstado())) {
+						existeTermoAtivo = true;						
+					}
+				}
+			}
+			request.setAttribute("existeTermoAtivo", existeTermoAtivo);
+			request.setAttribute("listaTermoEstagio", aluno.getTermoEstagios());
+			request.setAttribute("msg", msg);
 		}
-		
-                Aluno aluno = AlunoServices.buscarAluno(new Aluno(id));
-		List<TermoEstagio> termoEstagios =  aluno.getTermoEstagios();
-		
-		//TODO consertar a lógica de mensagem vazia
-		if(msg != "") {
-			aluno = AlunoServices.buscarAluno(new Aluno(id));			
-			termoEstagios = aluno.getTermoEstagios();
-		}
-		
-                if (termoEstagios != null) {
-                  //request.setAttribute("termosAditivos",TermoAditivoServices.listarTermoAditivo());
-                  if(aluno.getTermoEstagios().size() > 0){
-                	 List<TermoAditivo>termoAditivos = aluno.getTermoEstagios().get(aluno.getTermoEstagios().size()-1).getTermosAditivos();
-                	 Collections.sort(termoAditivos);
-                      request.setAttribute("termosAditivos", termoAditivos);
-                  }
-                }
-		
-                request.setAttribute("listaTermoEstagio", aluno.getTermoEstagios());
-                request.setAttribute("msg",msg);
-                }
 		request.getRequestDispatcher("/form_termo_aditivo.jsp").forward(request, response);
 
 	}
