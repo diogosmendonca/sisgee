75398b54b69e139d0f0232061452d2eba6e84a95
Paulo Eduardo Cantuária <padu77@gmail.com>
2019-09-17 03:35:27 -0300

    Add files via upload
    Ajuste na camada de controle

---
+++ sisgee/src/main/java/br/cefetrj/sisgee/control/CursoServices.java
@@ -0,0 +1,29 @@
+package br.cefetrj.sisgee.control;
+
+import br.cefetrj.sisgee.model.dao.CursoDAO;
+import br.cefetrj.sisgee.model.dao.PersistenceManager;
+import br.cefetrj.sisgee.model.entity.Curso;
+
+/**
+ * Serviços de alunos. Trata a lógica de negócios
+ * associada com a entidade Curso.
+ * 
+ * @author Paulo Cantuária
+ * 
+ */
+public class CursoServices {
+			
+        /**
+         * Método que inclui um curso
+         * @param curso 
+         */
+	public static void incluirAluno(Curso curso){		
+		PersistenceManager.getTransaction().begin();
+		try{
+			new CursoDAO().incluir(curso);
+			PersistenceManager.getTransaction().commit();
+		}catch(Exception e){
+			PersistenceManager.getTransaction().rollback();
+		}
+	}
+}
